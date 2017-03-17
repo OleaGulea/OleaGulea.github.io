ba1eb5d9072c8910dbf67072e91769525862b040
Olea Gulea <olea.sereda@gmail.com>
2017-03-17 20:49:21 +0200

    Button functionality only

--- js/template.js
+++ js/template.js
@@ -1,36 +1,14 @@
 /* global TrelloPowerUp */
 
-var WHITE_ICON = './images/icon-white.svg';
 var GRAY_ICON = './images/icon-gray.svg';
-
-var colorMap = {
-  black: 'black',
-  red: 'red'
-};
 
 
 var cardButtonCallback = function(t){
-  var colors = Object.keys(colorMap).map(function(colorCode){
-    return {
-      text: colorMap[colorCode],
-      callback: function(t){
-        return t.attach({ name: colorMap[colorCode] })
-        .then(function(){
-          return t.closePopup();
-        })
-      }
-    };
-  });
-
-  return t.popup({
-    title: 'Choose color',
-    items: colors,
-    search: {
-      count: 5,
-      placeholder: 'Choose color',
-      empty: 'No parks found'
+  return {
+    callback: function(t){
+      return t;
     }
-  });
+  };
 };
 
 TrelloPowerUp.initialize({
@@ -40,12 +18,5 @@ TrelloPowerUp.initialize({
       text: 'Template',
       callback: cardButtonCallback
     }];
-  },
-  'show-settings': function(t, options){
-    return t.popup({
-      title: 'Settings',
-      url: './settings.html',
-      height: 184
-    });
   }
 });
